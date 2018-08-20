db8f122b0a5d0983a23874fc5f515ce8f320f2c4
Arnaud J <arnaudj@users.noreply.github.com>
2018-08-20 17:55:24 +0200

    RFT Rely on component state and props passing instead of external object

--- src/katas/react-router/001-react-router-with-passed-props.js
+++ src/katas/react-router/001-react-router-with-passed-props.js
@@ -2,18 +2,10 @@ import ReactDOM from "react-dom";
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-const store = {
-  stories: [
-    { id: 1000, title: "A story", author: "john" },
-    { id: 1001, title: "Another story", author: "jane" }
-  ],
-  comments: [{ id: 5000, storyId: 1000, comment: "A comment", author: "jake" }],
-
-  getComments: function(storyId) {
-    return this.comments.filter(comment => comment.storyId === Number(storyId)); // workaround router param is string...
-  }
-};
-
+/*
+Pitfalls:
+Number(): workaround router param is string...
+*/
 const Home = () => (
   <div>
     <h2>Home</h2>
@@ -23,74 +15,112 @@ const Home = () => (
 class Story extends React.Component {
   render() {
     const {
-      store,
+      stories,
+      comments,
       match: {
         params: { storyId }
       }
     } = this.props;
     return (
       <div>
-        <h3>Reading story {storyId}</h3>
+        <h3>
+          Reading story {storyId}:{" "}
+          {stories.filter(story => story.id === Number(storyId))[0].title}
+          <br />
+        </h3>
         <div>
-          Story comments:
-          {store.getComments(storyId).map(comment => (
-            <div>
-              - {comment.author}: {comment.comment}
-              <br />
-            </div>
-          ))}
+          Story comments:<br />
+          {comments
+            .filter(comment => comment.storyId === Number(storyId))
+            .map(comment => (
+              <div key={comment.id}>
+                - {comment.author}: {comment.comment}
+                <br />
+              </div>
+            ))}
         </div>
       </div>
     );
   }
 }
 
-const StoriesList = ({ match, store }) => (
+const StoriesList = ({ baseUrl, stories }) => (
   <div>
     <h2>Stories</h2>
     <div>
-      {store.stories.map(story => (
-        <div>
-          <Link to={`${match.url}/${story.id}`}>{story.title}</Link>
+      {stories.map(story => (
+        <div key={story.id}>
+          <Link to={`${baseUrl}/${story.id}`}>{story.title}</Link>
         </div>
       ))}
     </div>
   </div>
 );
 
-const Stories = ({ match, store }) => (
+const Stories = ({ match: { url }, stories, comments }) => (
   <div>
     <Route
       exact
-      path={`${match.url}/`}
-      render={props => <StoriesList {...props} store={store} />}
+      path={`${url}/`}
+      render={props => <StoriesList baseUrl={url} stories={stories} />}
     />
     <Route
-      path={`${match.url}/:storyId`}
-      render={props => <Story {...props} store={store} />}
+      path={`${url}/:storyId`}
+      render={props => (
+        <Story {...props} stories={stories} comments={comments} />
+      )}
     />
   </div>
 );
 
-const BasicExample = () => (
-  <Router>
-    <div>
-      <Link to="/">Home</Link>
-      <br />
-      <Link to="/stories">Stories</Link>
-      <hr />
+class BasicExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stories: [
+        { id: 1000, title: "A story", author: "john" },
+        { id: 1001, title: "Another story", author: "jane" }
+      ],
+      comments: [
+        { id: 5000, storyId: 1000, comment: "A comment", author: "jake" }
+      ]
 
-      <Route exact path="/" component={Home} />
+      /*getComments: function(storyId) {
+        return this.comments.filter(
+          comment => comment.storyId === Number(storyId)
+        ); // workaround router param is string...
+      }*/
+    };
+  }
 
-      {/* No props forwarding, use render instead - https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/api/Route.md#render-func
+  render() {
+    return (
+      <Router>
+        <div>
+          <Link to="/">Home</Link>
+          <br />
+          <Link to="/stories">Stories</Link>
+          <hr />
+
+          <Route exact path="/" component={Home} />
+
+          {/* No props forwarding, use render instead - https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/api/Route.md#render-func
       https://github.com/ReactTraining/react-router/blob/master/packages/react-router/modules/Route.js#L120 */}
-      <Route
-        path="/stories"
-        render={props => <Stories store={store} {...props} />}
-      />
-    </div>
-  </Router>
-);
+          <Route
+            path="/stories"
+            render={props => (
+              <Stories
+                {...props}
+                stories={this.state.stories}
+                comments={this.state.comments}
+              />
+            )}
+          />
+        </div>
+      </Router>
+    );
+  }
+}
 
 ReactDOM.render(<BasicExample />, document.getElementById("root"));
 
